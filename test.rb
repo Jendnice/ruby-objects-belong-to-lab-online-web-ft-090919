9fec9608775e57e38b7477016904814305c891e3
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-21 19:36:07 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -17,5 +17,12 @@ class Post
   
 end 
 
+roald dahl = Author.new
+hotline_bling = Song.new("Hotline Bling")
+hotline_bling.artist = drake 
+
+puts hotline_bling.artist.genre
+puts hotline_bling.artist.name 
+
 "Roald Doahl" = Author.new
 puts Author 
